a68fce799e26bfec59cb60e9e57e4beab6c1e281
Mikkel Friis <mikkelbfriis@hotmail.com>
2024-07-23 10:26:40 +0200

    fix(session): refactor for async ping

--- Turbo.Networking/Game/Clients/SessionManager.cs
+++ Turbo.Networking/Game/Clients/SessionManager.cs
@@ -4,24 +4,23 @@ using System.Threading.Tasks;
 using DotNetty.Transport.Channels;
 using Turbo.Core.Networking.Game.Clients;
 using Turbo.Core.Packets;
+using Turbo.Networking.Clients;
 using Turbo.Packets.Incoming.Handshake;
 using Turbo.Packets.Outgoing.Handshake;
 
-namespace Turbo.Networking.Clients;
+namespace Turbo.Networking.Game.Clients;
 
 public class SessionManager : ISessionManager
 {
-    private const int _pingIntervalSeconds = 30;
     private readonly ConcurrentDictionary<IChannelId, ISession> _clients;
-    private readonly IPacketMessageHub _packetHub;
+    private const int _pingIntervalSeconds = 30;
     private long _lastPingSeconds;
 
     public SessionManager(IPacketMessageHub packetHub)
     {
-        _packetHub = packetHub;
         _clients = new ConcurrentDictionary<IChannelId, ISession>();
 
-        _packetHub.Subscribe<PongMessage>(this, OnPongMessage);
+        packetHub.Subscribe<PongMessage>(this, OnPongMessage);
     }
 
     public bool TryGetSession(IChannelId id, out ISession session)
@@ -34,9 +33,45 @@ public class SessionManager : ISessionManager
         return _clients.TryAdd(id, session);
     }
 
-    public void DisconnectSession(IChannelId id)
+    public async void DisconnectSession(IChannelId id)
     {
-        if (_clients.TryRemove(id, out var session)) session.DisposeAsync();
+        if (!_clients.TryRemove(id, out var session)) return;
+            
+        await session.DisposeAsync();
+    }
+
+    /// <summary>
+    /// Pings sessions every 30 seconds and disconnects sessions
+    /// that have timed out for 60 seconds.
+    /// </summary>
+    private async void ProcessPing()
+    {
+        var timeNow = DateTimeOffset.Now.ToUnixTimeSeconds();
+
+        if (timeNow - _lastPingSeconds < _pingIntervalSeconds) return;
+            
+        var tasks = new ConcurrentBag<Task>();
+
+        foreach (var session in _clients.Values)
+        {
+            if (timeNow - session.LastPongTimestamp > 60)
+            {
+                tasks.Add(session.DisposeAsync().AsTask());
+
+                continue;
+            }
+
+            await session.Send(new PingMessage());
+        }
+
+        await Task.WhenAll(tasks);
+            
+        _lastPingSeconds = timeNow;
+    }
+
+    private void OnPongMessage(PongMessage message, ISession session)
+    {
+        session.LastPongTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
     }
 
     public Task Cycle()
@@ -44,35 +79,5 @@ public class SessionManager : ISessionManager
         ProcessPing();
 
         return Task.CompletedTask;
-    }
-
-    /// <summary>
-    ///     Pings sessions every 30 seconds and disconnects sessions
-    ///     that have timed out for 60 seconds.
-    /// </summary>
-    private void ProcessPing()
-    {
-        var timeNow = DateTimeOffset.Now.ToUnixTimeSeconds();
-
-        if (timeNow - _lastPingSeconds < _pingIntervalSeconds) return;
-
-        foreach (var session in _clients.Values)
-        {
-            if (timeNow - session.LastPongTimestamp > 60)
-            {
-                session.DisposeAsync();
-
-                continue;
-            }
-
-            session.Send(new PingMessage());
-        }
-
-        _lastPingSeconds = timeNow;
-    }
-
-    public static void OnPongMessage(PongMessage message, ISession session)
-    {
-        session.LastPongTimestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
     }
 }
